d00419418ccbd2c20aa16ebbeb9826b5a0398aeb
ankit <ankitdwivedi1310@gmail.com>
2022-04-21 11:35:55 +0530

    another commit

--- LetterCombinationOfPhoneNumber.java
+++ LetterCombinationOfPhoneNumber.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Atoi {
+public class LetterCombinationOfPhoneNumber {
     final static char[][] L = {{}, {}, {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'},
             {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
 
